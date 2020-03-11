0aa84206e24d891354239825fc9dca1a07ea40d2
Wesley Chen <weschen1996@gmail.com>
2020-03-11 16:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your solution in this file!
 function updateDriverWIthKeyAndValue(object, key, value){
-  objcet[key] = value;
+  let newObj = object
+  newobject[key] = value
+  return newObj
 }
